3504aac224a9bd3842f1c4002cefc0ac5761c1f5
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2022-04-14 09:31:25 +0300

    Build script: use version 16 + RequiresOptIn

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,8 @@ dependencies {
 }
 
 tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "16"
+    kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
 }
 
 compose.desktop {
